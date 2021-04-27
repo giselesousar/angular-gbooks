751324b0bd61bbc7de371191e84d22b84eddc9c0
giselesousar <giselesousar@gmail.com>
2021-04-26 22:54:01 -0300

    create mocks

---
+++ src/app/mocks/books.ts
@@ -0,0 +1,167 @@
+export default [
+    {
+        "id": "y4AZ-k-zUskC",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/y4AZ-k-zUskC",
+        "volumeInfo": {
+            "title": "Note on Angular Distributions in Nuclear Reactions",
+            "authors": [
+                "E. Eisner",
+                "R. G. Sachs"
+            ],
+            "publishedDate": "1947",
+            "pageCount": 6,
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=y4AZ-k-zUskC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=y4AZ-k-zUskC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+        },
+        "saleInfo": {
+            "buyLink": "https://play.google.com/store/books/details?id=y4AZ-k-zUskC&rdid=book-y4AZ-k-zUskC&rdot=1&source=gbs_api"
+        },
+    },
+    {
+        "id": "iXAYqzeP_wsC",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/iXAYqzeP_wsC",
+        "volumeInfo": {
+            "title": "A Nonlinear Estimator for Reconstructing the Angular Velocity of a Spacecraft Without Rate Gyros",
+            "authors": [
+                "Michael Edward Polites"
+            ],
+            "publishedDate": "1991",
+            "pageCount": 19,
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=iXAYqzeP_wsC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=iXAYqzeP_wsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+        },
+        "saleInfo": {
+            "buyLink": "https://play.google.com/store/books/details?id=iXAYqzeP_wsC&rdid=book-iXAYqzeP_wsC&rdot=1&source=gbs_api"
+        },
+    },
+    {
+        "id": "y4AZ-k-zUskC",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/y4AZ-k-zUskC",
+        "volumeInfo": {
+            "title": "Note on Angular Distributions in Nuclear Reactions",
+            "authors": [
+                "E. Eisner",
+                "R. G. Sachs"
+            ],
+            "publishedDate": "1947",
+            "pageCount": 6,
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=y4AZ-k-zUskC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=y4AZ-k-zUskC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+        },
+        "saleInfo": {
+            "buyLink": "https://play.google.com/store/books/details?id=y4AZ-k-zUskC&rdid=book-y4AZ-k-zUskC&rdot=1&source=gbs_api"
+        },
+    },
+    {
+        "id": "iXAYqzeP_wsC",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/iXAYqzeP_wsC",
+        "volumeInfo": {
+            "title": "A Nonlinear Estimator for Reconstructing the Angular Velocity of a Spacecraft Without Rate Gyros",
+            "authors": [
+                "Michael Edward Polites"
+            ],
+            "publishedDate": "1991",
+            "pageCount": 19,
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=iXAYqzeP_wsC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=iXAYqzeP_wsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+        },
+        "saleInfo": {
+            "buyLink": "https://play.google.com/store/books/details?id=iXAYqzeP_wsC&rdid=book-iXAYqzeP_wsC&rdot=1&source=gbs_api"
+        },
+    },
+    {
+        "id": "y4AZ-k-zUskC",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/y4AZ-k-zUskC",
+        "volumeInfo": {
+            "title": "Note on Angular Distributions in Nuclear Reactions",
+            "authors": [
+                "E. Eisner",
+                "R. G. Sachs"
+            ],
+            "publishedDate": "1947",
+            "pageCount": 6,
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=y4AZ-k-zUskC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=y4AZ-k-zUskC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+        },
+        "saleInfo": {
+            "buyLink": "https://play.google.com/store/books/details?id=y4AZ-k-zUskC&rdid=book-y4AZ-k-zUskC&rdot=1&source=gbs_api"
+        },
+    },
+    {
+        "id": "iXAYqzeP_wsC",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/iXAYqzeP_wsC",
+        "volumeInfo": {
+            "title": "A Nonlinear Estimator for Reconstructing the Angular Velocity of a Spacecraft Without Rate Gyros",
+            "authors": [
+                "Michael Edward Polites"
+            ],
+            "publishedDate": "1991",
+            "pageCount": 19,
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=iXAYqzeP_wsC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=iXAYqzeP_wsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+        },
+        "saleInfo": {
+            "buyLink": "https://play.google.com/store/books/details?id=iXAYqzeP_wsC&rdid=book-iXAYqzeP_wsC&rdot=1&source=gbs_api"
+        },
+    },
+    {
+        "id": "y4AZ-k-zUskC",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/y4AZ-k-zUskC",
+        "volumeInfo": {
+            "title": "Note on Angular Distributions in Nuclear Reactions",
+            "authors": [
+                "E. Eisner",
+                "R. G. Sachs"
+            ],
+            "publishedDate": "1947",
+            "pageCount": 6,
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=y4AZ-k-zUskC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=y4AZ-k-zUskC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+        },
+        "saleInfo": {
+            "buyLink": "https://play.google.com/store/books/details?id=y4AZ-k-zUskC&rdid=book-y4AZ-k-zUskC&rdot=1&source=gbs_api"
+        },
+    },
+    {
+        "id": "iXAYqzeP_wsC",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/iXAYqzeP_wsC",
+        "volumeInfo": {
+            "title": "A Nonlinear Estimator for Reconstructing the Angular Velocity of a Spacecraft Without Rate Gyros",
+            "authors": [
+                "Michael Edward Polites"
+            ],
+            "publishedDate": "1991",
+            "pageCount": 19,
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=iXAYqzeP_wsC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=iXAYqzeP_wsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+        },
+        "saleInfo": {
+            "buyLink": "https://play.google.com/store/books/details?id=iXAYqzeP_wsC&rdid=book-iXAYqzeP_wsC&rdot=1&source=gbs_api"
+        },
+    },
+
+]
